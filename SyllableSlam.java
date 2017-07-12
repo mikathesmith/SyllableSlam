e1ed9f0bbedcc57e488a8bdbd884da542465c79b
Mika Smith <mismith@oucs1449.otago.ac.nz>
2017-07-12 16:39:45 +1200

    Fixed up code for lcases where last letters are 'le' or 'les' by putting it outside the if(isVowel)

--- SyllableSlam.java
+++ SyllableSlam.java
@@ -23,7 +23,7 @@ public class SyllableSlam{
 		while(sc.hasNext()){
 			String word = sc.next();
 			int syllableFreq = estimateSyllables(word);
-			System.out.println(syllableFreq);
+			System.out.println(word + " " +syllableFreq);
 		}
 		sc.close(); 
 	}
@@ -37,24 +37,39 @@ public class SyllableSlam{
 		
 		for(int i=0; i<word.length();i++){
 			char c = word.charAt(i);
-			if(isVowel(c)){	
-				if(word.length() > 2){
-					if(i == word.length()-1 && c=='e'){
-						//add syllable if word ends in le and letter before l is a consonant
-						if(word.charAt(i-2) == 'l' && !isVowel(word.charAt(i-3))) {
-							count++;
-						}else{
-							break;
-						}
-					}else if(i == word.length()-1 && c=='s'){
-						//add syllable if word ends in les and letter before l is a consonant
-						if(word.charAt(i-2) == 'e' && word.charAt(i-2) == 'l' && !isVowel(word.charAt(i-3))) {
-							count++;
-						}else {
-							break;
-						}
-					}
+			
+			if(i==word.length()-1 && word.length()>2 && (c=='e' || c=='s')){
+				
+				if(c=='e' && word.charAt(i-1) == 'l' && !isVowel(word.charAt(i-2))) {
+					//System.out.println("last letter is Xle where X is a consonant!");
+					count++;
+				}else if(c=='s' && word.charAt(i-1) == 'e' && word.charAt(i-2) == 'l' && !isVowel(word.charAt(i-3))){
+				//	System.out.println("last letter is Xles where X is a consonant!");
+					count++;
+				}else{
+					break;
 				}
+			}else if(isVowel(c)){	
+				//if(word.length() > 2){
+					//System.out.println("here!");
+					//if(i == word.length()-1 && c=='e'){ 
+						//add syllable if word ends in le and letter before l is a consonant
+					//	if(word.charAt(i-2) == 'l' && !isVowel(word.charAt(i-3))) {
+					//		count++;
+							//System.out.println("here!");
+						//}else{
+						//	break;
+						//}
+					//}
+					//else if(i == word.length()-1 && c=='s'){
+						//add syllable if word ends in les and letter before l is a consonant
+					//	if(word.charAt(i-2) == 'e' && word.charAt(i-2) == 'l' && !isVowel(word.charAt(i-3))) {
+					//		count++;
+					//	}else {
+					//		break;
+					//	}
+					//}
+				//}
 				if(lastCharVowel==true){ //Two vowels in a row	
 					//if the last char is the same as this char
 					if(c != (word.charAt(i-1))){
@@ -66,8 +81,9 @@ public class SyllableSlam{
 				}
 				lastCharVowel=true;
 			}else{
-				lastCharVowel =false; 
-			}		
+				lastCharVowel =false;
+			}
+				
 		}
 		return count==0 ? 1 : count;
 	}
